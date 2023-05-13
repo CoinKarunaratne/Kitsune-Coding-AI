ad6aba1526f9773e81f2302eaf00f4e55227d59a
A Karunaratne <coinkarunaratne@gmail.com>
2023-05-13 21:18:45 +1200

    implemented AI

--- client/script.js
+++ client/script.js
@@ -21,7 +21,7 @@ function typeText(element, text) {
 
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerHTML += text.chartAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -53,7 +53,7 @@ function chatStripe(isAi, value, uniqueId) {
   `;
 }
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
 
   const data = new FormData(form);
@@ -73,6 +73,31 @@ function handleSubmit(e) {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+  }
 }
 
 form.addEventListener("submit", handleSubmit);
