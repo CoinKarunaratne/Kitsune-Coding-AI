da5b26efbc343d53ae0e120baf1abeff7fa4eb7b
A Karunaratne <coinkarunaratne@gmail.com>
2023-05-13 21:42:49 +1200

    added live server

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ async function handleSubmit(e) {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://kitsune-ai.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
